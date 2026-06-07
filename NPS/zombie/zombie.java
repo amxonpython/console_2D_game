e54ae976dc8291b2ed45cf4a9786f60979700202
lampa <134073971+amxonpython@users.noreply.github.com>
2026-06-07 16:37:39 +0300

    Update zombie.java

--- NPS/zombie/zombie.java
+++ NPS/zombie/zombie.java
@@ -1,9 +1,10 @@
 package NPS.zombie;
-import objects.data.data;
 import player.*;
 import world.*;
 
 public class zombie {
+    public int coordinate_x_pub = 0;
+    public int coordinate_y_pub = 0;
 
     public int Attack_power = 1;
 
@@ -11,40 +12,30 @@ public class zombie {
         parameters_player.damag(Attack_power);
     }
 
-    public void Spawn(boolean NPC, int coordinate_y, int coordinate_x, int number){
-        if (!data.Was_there_a_tick_in_the_past_z[number]){
-            data.coordinate_x_z[number] = coordinate_x;
-            data.coordinate_y_z[number] = coordinate_y;
-            data.Was_there_a_tick_in_the_past_z[number] = true;
-        }
+    public void Spawn(int coordinate_y, int coordinate_x){
+        matrix.setka[coordinate_y][coordinate_x] = "&";
+        coordinate_y_pub = coordinate_y;
+        coordinate_x_pub = coordinate_x;
+    }
+    public void NPS(){
+        matrix.setka[coordinate_y_pub][coordinate_x_pub] = "&";
 
-        coordinate_x = data.coordinate_x_z[number];
-        coordinate_y = data.coordinate_y_z[number];
+        if (coordinate_x_pub == player.coordinate_x && coordinate_y_pub == player.coordinate_y){
+            attack();
+        }else {
+            if (coordinate_x_pub > player.coordinate_x && coordinate_x_pub - 1 >= 0) {
+                coordinate_x_pub--;
+            }
+            if (coordinate_x_pub < player.coordinate_x && coordinate_x_pub + 1 <= world.size){
+                coordinate_x_pub++;
+            }
 
-        if (NPC){
-            matrix.setka[data.coordinate_y_z[number]][data.coordinate_x_z[number]] = "&";
-
-            if (coordinate_x == player.coordinate_x && coordinate_y == player.coordinate_y){
-                attack();
-            }else {
-                if (coordinate_x > player.coordinate_x && coordinate_x - 1 >= 0) {
-                    coordinate_x--;
-                }
-                if (coordinate_x < player.coordinate_x && coordinate_x + 1 <= world.size){
-                    coordinate_x++;
-                }
-
-                if (coordinate_y > player.coordinate_y && coordinate_y - 1 >= 0){
-                    coordinate_y--;
-                }
-                if (coordinate_y < player.coordinate_y && coordinate_y + 1 <= world.size) {
-                    coordinate_y++;
-                }
+            if (coordinate_y_pub > player.coordinate_y && coordinate_y_pub - 1 >= 0){
+                coordinate_y_pub--;
+            }
+            if (coordinate_y_pub < player.coordinate_y && coordinate_y_pub + 1 <= world.size) {
+                coordinate_y_pub++;
             }
         }
-        data.coordinate_x_z[number] = coordinate_x;
-        data.coordinate_y_z[number] = coordinate_y;
-        data.Was_there_a_tick_in_the_past_z[number] = true;
-
     }
 }
